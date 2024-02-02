76846b1dc3f8cc7625c3c0293f8f6d4a46575aeb
unknown <tavata.canale@gmail.com>
2024-02-02 16:05:21 -0500

    mostrar producto por slug

---
+++ src/actions/products/get-product-by-slug.ts
@@ -0,0 +1,29 @@
+import { productfindmanyinterface } from "@/interfaces";
+import prisma from '@/lib/prisma';
+import { ProductImage } from "@prisma/client";
+
+
+export const getproductbyslug = async (slug: string) => {
+    try {
+        const product: productfindmanyinterface = await prisma.product.findFirst({
+            include: {
+                ProductImage: {
+                    select: {
+                        url: true
+                    }
+                }
+            }, where: {
+                slug: slug
+            }
+        })
+        if (!product) return null;
+        return {
+            ...product,
+            images: product.ProductImage.map((image) => image.url)
+        }
+    } catch (error) {
+        console.log(error)
+        throw new Error('Error al obtener producto por slug')
+    }
+}
+
