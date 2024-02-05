cc485993b674e7f6644c64a58f0f3f1e4688a69a
unknown <tavata.canale@gmail.com>
2024-02-04 23:45:43 -0500

    zod instalado

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,11 +1,25 @@
 import type { NextAuthConfig } from 'next-auth';
- 
-export const authConfig:NextAuthConfig = {
-  pages: {
-    signIn: '/auth/login',
-    newUser:'/auth/new-account'
-  },
-  providers:[
-    
-  ]
+import NextAuth from 'next-auth';
+import credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+export const authConfig: NextAuthConfig = {
+    pages: {
+        signIn: '/auth/login',
+        newUser: '/auth/new-account'
+    },
+    providers: [
+        credentials({
+            async authorize(credentials) {
+                const parsedCredentials = z
+                    .object({ email: z.string().email(), password: z.string().min(6) })
+                    .safeParse(credentials);
+
+                if (!parsedCredentials.success) return null;
+                const { email, password } = parsedCredentials.data
+                console.log({ email, password })
+                return null;
+            },
+        }),
+    ]
 };
+export const { signIn, signOut, auth } = NextAuth(authConfig)
