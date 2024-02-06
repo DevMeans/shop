2bfb43d7a0da7d24a488dac857f1cc72786580f6
unknown <tavata.canale@gmail.com>
2024-02-05 22:27:48 -0500

    Se añadio provider al shop/layout

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,6 +1,6 @@
 import { TopMenu } from '../../components/ui/top-menu/TopMenu';
 import { Sidebar } from '../../components/ui/sidebar/Sidebar';
-import { Footer } from '@/components';
+import { Footer, Provider } from '@/components';
 
 export default function ShopLayout({
   children
@@ -8,13 +8,17 @@ export default function ShopLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-screen">
-      <TopMenu></TopMenu>
-      <Sidebar />
-      <div className='px-0 sm:px-10'>
-        {children}
-      </div>
-      <Footer />
-    </main>
+    <Provider>
+
+
+      <main className="min-h-screen">
+        <TopMenu></TopMenu>
+        <Sidebar />
+        <div className='px-0 sm:px-10'>
+          {children}
+        </div>
+        <Footer />
+      </main>
+    </Provider> //TODO:aca le puesto un provider no que tanto sera bueno añadirle pero esta bueno preguntar
   );
 }
